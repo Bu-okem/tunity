ad18adc1536cc020bcbbf5bed34348b60a0798ed
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-10-16 20:19:37 +0100

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <h2 className="logo">Tunity</h2>
+      <div className="mobile-menu">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Nav from '../components/Nav';
+
+import '../styles/App.scss';
+
+const Home = () => {
+  return (
+    <section className="home">
+      <Nav />
+    </section>
+  );
+};
+
+export default Home;
